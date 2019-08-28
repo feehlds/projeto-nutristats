f5d9c113bc2b21fba1cce9a63fec883f3e6141ae
felipe <felipe-loureiro@outlook.com>
2019-08-28 10:46:44 -0300

    Teste

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,'/HTML/index.html'))
+})
+
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname,'/HTML/index.html'))
 });

1867eec543d9c6b776320f71ec31e79b8d0d9189
felipe <felipe-loureiro@outlook.com>
2019-08-28 10:55:46 -0300

    Teste

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const http = require('http')
+const port = process.env.PORT || 3000
 
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname,'/HTML/index.html'))
-})
-
-app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname,'/HTML/index.html'))
+const server = http.createServer((req, res) =>{
+    res.statusCode = 200
+    res.setHeader("Content-Type", 'text/html')
+    res.end("<h1>Hello world</h1>")
+});
+server.listen(port, () => {
+    console.log('Server running on port ' + port);
 });

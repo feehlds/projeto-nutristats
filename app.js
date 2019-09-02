474d68b60c119a841715f7a6b374b13839a99cc4
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-09-01 21:08:11 -0300

    Tentando conexão db

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ app.use(express.static(__dirname))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/html/index.html'))
 });
+
+//Tentando fazer conexão com o banco de dados
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM nutrientes;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});

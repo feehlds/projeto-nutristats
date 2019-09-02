02b65ec6e02e9095a0896578156f891edeb8af14
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-09-01 21:39:40 -0300

    Testanto busca no banco de dados

---
+++ js/pesquisaNutrientes.js
@@ -0,0 +1,9 @@
+function pesquisar(string1){
+    client.query('SELECT * FROM nutrientes WHERE Descricao LIKE _' + string1 +'_ ;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          document.write(JSON.stringify(row))
+        }
+        client.end();
+      });
+}

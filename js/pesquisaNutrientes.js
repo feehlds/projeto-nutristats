c3a78c132ca6db01060a484aaeb8c062ec869d80
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-09-01 21:59:29 -0300

    Testanto busca no banco de dados 4

--- js/pesquisaNutrientes.js
+++ js/pesquisaNutrientes.js
@@ -1,3 +1,11 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 function pesquisar(string1){
     client.query('SELECT * FROM nutrientes WHERE Descricao LIKE _' + string1 +'_ ;', (err, res) => {
         if (err) throw err;

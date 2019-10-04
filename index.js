9b79968738c23d64a2a4c35b3280962e6202ea96
Luana Tavares Faiotto <luana.faiotto@gmail.com>
2019-10-04 11:19:21 -0300

    Commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3030
 
 //Chamando conexão com o banco de dados
-//const client = require('./src/models/database/conexao');
+const client = require('./src/models/database/conexao');
 
 var path = require('path');
 

d198bf123a00dd89ab9481b977e95ca181e6d915
felipe <floureiro@betait.com.br>
2019-10-19 01:03:03 -0300

    Fazendo busca bd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Importação
     //Requisição da biblioteca express
     const express = require('express');
+    const pesqAlimentos = require('./src/models/database/pesqAlimentos')
     //Definindo porta padrão ou 3030
     const PORT = process.env.PORT || 3030
     //normalizando path
@@ -13,9 +14,16 @@
     app.use(express.static(path.join(__dirname, 'public')));
     
     //requests e responses
-    app.get('/', (req, res) => {
+    app.get('/', (req, res, next) => {
         res.sendFile(path.join(__dirname,'public/html/index.html'))
        // res.render("index");
+    });
+
+    app.get('/pesqAlimentos', (req, res) => {
+        console.log(req.query);
+        let pesq = req.query;
+        console.log(pesq.barraPesq);
+        pesqAlimentos.pesqAlimentos(pesq.barraPesq);
     });
 
     // O app Listen sempre deve ser a ultima linha do código
--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -1,11 +1,14 @@
 client = require ('./conexao');
 
-client.connect();
-
 function pesqAlimento(string){
     client.connect();
     client.query('SELECT * from nutrientes WHERE descricao LIKE %?%', [string], (err, res) => {
         if (err) throw err;
+
+        for(let row in res.rows)
+            console.log(row)
         return res.rows;
     }); 
 }
+
+module.exports = pesqAlimento();

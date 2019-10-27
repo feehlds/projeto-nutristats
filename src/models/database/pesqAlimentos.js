d362d853d286604fab55832df2bb86fcfbe3b26b
felipe <floureiro@betait.com.br>
2019-10-27 15:41:14 -0300

    New search query

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -2,6 +2,8 @@ client = require ('./conexao');
 
 function pesqAlimento(string){
     let regexp = "(.*\s)?"+ string.toLowerCase(); +"\s?.*')";
+    console.log('THIS IS THE REGEX: ' + regexp);
+    Console.log("SELECT *, (regexp_matches(lower(descricao), ?)) from nutrientes;", [regexp])
     client.query("SELECT *, (regexp_matches(lower(descricao), ?)) from nutrientes;", [regexp], (err, res) => {
     if (err) console.log(err);
     for(let row of res.rows){

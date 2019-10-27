08f41a8c97467218a8db2f0a9d15596615da9227
felipe <floureiro@betait.com.br>
2019-10-27 19:54:53 -0300

    sql fix

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -1,8 +1,8 @@
 client = require ('./conexao');
 
 function pesqAlimento(string){
-    let regexp = "(.*\\s)?"+ string.toLowerCase() + "\\s?.*')";
-    client.query("SELECT *, (regexp_matches(lower(descricao), ?)) from nutrientes;", [regexp], (err, res) => {
+    let regexp = "'(.*\\s)?"+ string.toLowerCase() + "\\s?.*'";
+    client.query('SELECT *, (regexp_matches(lower(descricao), ?)) from nutrientes;', [regexp], (err, res) => {
     if (err) console.log(err);
     for(let row of res.rows){
         console.log(JSON.stringify(row));

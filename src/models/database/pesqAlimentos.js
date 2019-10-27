c9933b69733ba4c7c3c4eb2907c72892105c5942
felipe <floureiro@betait.com.br>
2019-10-27 20:31:49 -0300

    Refatorando método

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -1,7 +1,7 @@
 client = require ('./conexao');
 
 function pesqAlimento(string){
-    let regexp = "'(.*\\s)?"+ string.toLowerCase() + "\\s?.*'";
+    let regexp =  new RegExp("'(.*\s)?"+ string.toLowerCase() + "\s?.*'");
     client.query('SELECT *, (regexp_matches(lower(descricao), ?)) from nutrientes;', [regexp], (err, res) => {
     if (err) console.log(err);
     for(let row of res.rows){

eb18639ed6039615eadcbbb8c3dc7431b5d5f542
felipe <floureiro@betait.com.br>
2019-11-02 16:07:58 -0300

    commit

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -3,8 +3,8 @@ var client = require ('./conexao');
 function pesqAlimento(string){
     let str = string.toLowerCase();
     console.log(str);
-    client.connect()
-    client.query("SELECT * FROM pesquisaAlimentos('$1')", [str], (err, res) => {
+    client.connect();
+    client.query("SELECT * FROM pesquisaAlimentos('"+str+"')", (err, res) => {
     if (err) console.log(err);
     for(let row of res.rows){
         console.log(JSON.stringify(row));

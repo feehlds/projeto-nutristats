a40ba3764d704707d7ab0fd2feb339240d20a4f3
felipe <floureiro@betait.com.br>
2019-11-02 15:17:30 -0300

    selecting by view

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -3,13 +3,15 @@ var client = require ('./conexao');
 function pesqAlimento(string){
     let str = string.toLowerCase();
     console.log(str);
-    client.query("SELECT *, (regexp_matches(lower(descricao), '(.*\s)?" + str + "\s?.*')) from nutrientes;", (err, res) => {
+    client.connect()
+    client.query("SELECT * FROM pesquisaAlimentos('?')", [str], (err, res) => {
     if (err) console.log(err);
     for(let row of res.rows){
         console.log(JSON.stringify(row));
     }
-    console.log('consulta feita!'); 
-        return res.rows;
+    console.log('consulta feita!');
+    client.end(); 
+     return res.rows;
     }); 
 }
 

422b6853ea69a37a943b8b697c91060cead022fc
felipe <floureiro@betait.com.br>
2019-11-02 17:25:12 -0300

    refatorando método

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -2,14 +2,13 @@ var client = require ('./conexao');
 
 function pesqAlimento(string){
     let str = string.toLowerCase();
-    let result;
     client.connect();
     client.query("SELECT * FROM pesquisaAlimentos('"+str+"')", (err, res) => {
-    if (err) console.log(err); 
-    result = JSON.stringify(res.rows);
-    });
-    client.end();
-    return result 
+    if (err) console.log(err);
+    console.log(res.rows);
+    client.end(); 
+    return res.rows;
+    }); 
 }
 
 module.exports = pesqAlimento;

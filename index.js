5d272df7729a7ce795d6cc1fc6b2ff699c149abd
André Nascimento de Freitas <afreitas@gertec.local>
2019-11-05 18:32:38 -0200

    Crud mongo exclusão

--- index.js
+++ index.js
@@ -26,20 +26,21 @@
 
 
   //INSERIR USUARIO/ Atualizar
-            // var user = new usuario.Usuario();
-            //     //se for atualizar usar usar id id
-            //      user.setId("5dbcd4a2e7b987351c771b01")
-            // user.setNome("André");
-            // user.setDataNascimento("16/01/2000");
-        
-            // user.setNomeUsuario("André");
-            // user.setPeso(1.2);
-            // user.setSexo("M");
-            // user.setAltura(12552);
-            // user.setSenha('pedroasfadf');
-            // var usuarioPersistencia = new usuarioInserir.Usuario();
-            // //usuarioPersistencia.inserir(user);
-            // usuarioPersistencia.atualizar(user);
+            var user = new usuario.Usuario();
+                //se for atualizar usar usar id id
+               //  user.setId("5dbcd4a2e7b987351c771b01")
+            user.setNome("André");
+            user.setDataNascimento("16/01/2000");
+            user.setEmail('test@tete.com.br')
+            user.setNomeUsuario("André");
+            user.setPeso(1.2);
+            user.setSexo("M");
+            user.setAltura(12552);
+            user.setSenha('pedroasfadf');
+            var usuarioPersistencia = new usuarioInserir.Usuario();
+            usuarioPersistencia.excluir('5dc1b9927573d91da8cba166');
+            //usuarioPersistencia.atualizar(user);
+            
 
 
     app.get('/pesqAlimentos', (req, res) => {

a457561d78b02529bd9cf058aec3559a81bf9098
nfandre <andrelive.05@hotmail.com>
2019-11-08 12:40:57 -0200

    criando rota para cadastrar

--- src/routes/usuario/usuario.js
+++ src/routes/usuario/usuario.js
@@ -5,35 +5,44 @@ const UsuarioPers = require('../../models/persistencia/usuario');
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 
-// router.get("/registro", (req, res) =>{
-//     res.render("usuarios/registro");
-// });
+router.get("/", (req, res) =>{
+    res.render("usuarios/registro");
+});
 
 router.post("/registro", (req,res) => {
     var up = new UsuarioPers.Usuario();
-    console.log("entoru aqui")
     var usuarioNovo = new usuario.Usuario();
-    console.log("AAAAAAAAAA",req.body);
-    usuarioNovo.setNomeUsuario(req.body.nomeUsuario);
-    usuarioNovo.setSenha(req.body.senhaUsuario);
-    console.log(usuarioNovo.getNomeUsuario());
-    console.log(usuarioNovo.getSenha());
-    // bcrypt.genSalt(10, (erro, salt) => {
-    //     bcrypt.hash(usuarioNovo.getSenha(), salt, (erro, hash) =>{
-    //         if(erro){
-    //            // req.flash("error_msg", "Houve um erro durante o salvamento do usuario")
-    //             console.log("DEU ERRO", erro);
-    //         }
+ 
+    usuarioNovo.setNome(req.body.nomeUser);
+    usuarioNovo.setDataNascimento(req.body.dataNasc);
+    usuarioNovo.setEmail(req.body.email);
+    //usuarioNovo.setPeso(1.2);
+   // usuarioNovo.setAltura(12552);
+    usuarioNovo.setSexo(req.body.sexo);
+    usuarioNovo.setNomeUsuario(req.body.username);
 
-    //         usuarioNovo.setSenha(hash);
-    //         up.inserir(usuarioNovo).then(()=>{
-    //             res.redirect('/');
-    //         }).catch((err)=>{
-    //             console.log(err);
-    //         });
+    usuarioNovo.setSenha(req.body.senha);
+
+
+
+    console.log(usuarioNovo);
+    bcrypt.genSalt(10, (erro, salt) => {
+        bcrypt.hash(usuarioNovo.getSenha(), salt, (erro, hash) =>{
+            if(erro){
+               // req.flash("error_msg", "Houve um erro durante o salvamento do usuario")
+                console.log("DEU ERRO", erro);
+            }
+            usuarioNovo.setSenha(hash);
+            console.log("Senha: ",usuarioNovo.getSenha());
+            
+            // up.inserir(usuarioNovo).then(()=>{
+            //     res.redirect('/');
+            // }).catch((err)=>{
+            //     console.log(err);
+            // });
            
-    //     });
-    // });
+        });
+    });
     
 });
 

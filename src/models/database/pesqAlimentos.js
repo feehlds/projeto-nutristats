42c6d9047d11e8cdcac8f01bebeceefa52d1ef04
felipe <floureiro@betait.com.br>
2019-11-10 17:20:25 -0300

    merge 2

--- src/models/database/pesqAlimentos.js
+++ src/models/database/pesqAlimentos.js
@@ -1,19 +1,8 @@
 async function pesqAlimento(string){
     let str = string.toLowerCase();
     try{
-
-
         let pool = require ('./conexao'); 
         let res = await pool.query("SELECT * FROM pesquisaAlimentos('"+str+"')");
-
-        let client = require ('./conexao'); 
-        client.connect();
-        let res = await client.query("SELECT * FROM pesquisaAlimentos('"+str+"')");
-        await client.end();   
-
-        let pool = require ('./conexao'); 
-        let res = await pool.query("SELECT * FROM pesquisaAlimentos('"+str+"')");
-
         return res.rows;
     }   catch(err){
         throw(err);

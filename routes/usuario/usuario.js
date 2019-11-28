0080944abe3e83338d3c9e07aa3fcd4bb9176442
nfandre <andrelive.05@hotmail.com>
2019-11-28 12:02:32 -0200

    colocando mensagens de erro

--- routes/usuario/usuario.js
+++ routes/usuario/usuario.js
@@ -115,7 +115,7 @@ router.get("/login", (req,res) => {
 router.post("/login",(req,res,next)=>{
     passport.authenticate("local",{
         successRedirect: "/",
-        failureRedirect: "/erro",
+        failureRedirect: "/",
         failureFlash: true
     })(req,res,next);
     

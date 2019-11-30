c510868e447e84d4bce9155a7ecffcac3bb145e8
nfandre <andrelive.05@hotmail.com>
2019-11-29 22:13:08 -0300

    update usuario

--- routes/usuario/usuario.js
+++ routes/usuario/usuario.js
@@ -58,61 +58,36 @@ router.get("/atualizar", (req, res) =>{
     res.render("usuarios/atualizar")
 })
 router.post("/atualizar", (req,res) => {
-    var validar = true;
     //verifica se o usuário ja esta cadastrado no sitema
-    var nomeUsuario =  req.body.nomeUsuario;
-    var id = req.user.id;
+    let nomeUsuario =  req.body.nomeUsuario;
+    let id = req.user.id;
 
-    Usuario.find().then((usuarioList)=>{           
-        console.log(id)
-        console.log(req.user.nomeUsuario)
-        usuarioList.forEach (function (usuarioFind) { 
-
-            if(usuarioFind.nomeUsuario === nomeUsuario &&  usuarioFind.id !== id){
-                validar = false;
-                console.log(validar);
-                req.flash("error_msg", "Já existe uma conta com este nome de Usuário em nosso sistema");
-                res.redirect("/");
-            }else{
-
-               
-            }
-        }); 
+    Usuario.findOne({nomeUsuario: nomeUsuario}).then((usuario)=>{           
+          if(!usuario || usuario.id ===id){
+                    usuario.nome = req.body.nome;
+                    usuario.email = req.body.email;
+                    usuario.nomeUsuario =  req.body.nomeUsuario;
+                    usuario.senha =  usuario.senha;
+                    usuario.perfil = {
+                                        "DataNascimento":req.body.dataNasc,
+                                } 
+                    req.user.save().then(()=>{
+                        req.flash("success_msg", "Usuario criado com sucesso!")
+                        res.redirect("/");
+        
+                    }).catch((err) => {
+                        req.flash("error_msg", "Houve um erro interno");
+                        res.redirect("/");
+                    });
+          }else{
+            req.flash("error_msg", "Já existe uma conta com este nome de Usuário em nosso sistema");
+            res.redirect("/");
+          }
+        
     }).catch((err)=>{
         req.flash("error_msg", "Houve um erro interno ao editar o usuário");
         res.redirect("/");
     })
-    console.log(validar);
-
-    if(validar===true){
-        console.log("entrou aqui")
-        console.log(validar);
-            //Se não, realiza o update
-        Usuario.findOne({_id: req.user.id}).then((usuario)=>{
-            usuario.nome = req.body.nome;
-            usuario.email = req.body.email;
-            usuario.nomeUsuario =  req.body.nomeUsuario;
-            usuario.senha =  usuario.senha;
-            usuario.perfil = {
-                                "DataNascimento":req.body.dataNasc,
-                                } 
-                    
-            usuario.save().then(()=>{
-                req.flash("success_msg", "Usuario criado com sucesso!")
-                res.redirect("/");
-
-            }).catch((err) => {
-                req.flash("error_msg", "Houve um erro interno");
-                res.redirect("/");
-            });
-        }).catch((err)=>{
-            req.flash("error_msg", "Houve um erro ao atualizaro o usuário")
-            res.redirect("/");
-        }); 
-    }
-    
-
-
 });
 
 

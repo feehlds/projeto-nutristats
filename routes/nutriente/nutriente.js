62da341f9bef8556f0b6365239f9ab6cdbc18e98
nfandre <andrelive.05@hotmail.com>
2019-11-30 11:20:24 -0300

    pesquisa de alimentos

--- routes/nutriente/nutriente.js
+++ routes/nutriente/nutriente.js
@@ -6,11 +6,10 @@ const Nutriente = mongoose.model("nutrientes");
 
 
 router.get('/nutrientes/', (req, res) =>{
-    console.log(req.body.barraPesq);
-    Nutriente.find({Descricao: "/Feijao/i"}).then((nutrientes)=>{
-       
+    Nutriente.find({Descricao: new RegExp(req.body.barraPesq,'i')}).then((nutrientes)=>{   
+        
         console.log(nutrientes);
-        res.render("nutrientes/pesquisaNutriente", {nutrientes: nutrientes});
+        res.render("index", {nutrientes: nutrientes});
     }).catch((err)=>{
         req.flash("error_msg","houve um erro ao listar os nutrientes" );
         res.redirect("/");

71a6fd981efdea45a5c5d894c67aa37f6c7ead34
Felipe <felipe-loureiro@outlook.com>
2019-11-30 15:46:18 -0300

    Fixing ajax

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,47 @@
-//animações barra de pesquisa e botão de pesquisa
-document.getElementById('barraPesquisa').addEventListener('focus', function(){
-    document.getElementById('bt-buscar').style.width = '100%';
-});
-document.getElementById('barraPesquisa').addEventListener('focusout', function(){
-    document.getElementById('bt-buscar').style.width = '10rem';
-});
-
 //Chamada Foundation (Funcionamento do Modal - Foundation Reveal)
 $(document).foundation();
 
 //more click
-$('.card-profile-stats-more-link').click(function(e){
+$('.card-profile-stats-more-link').click(function (e) {
+  e.preventDefault();
+  if ($(".card-profile-stats-more-content").is(':hidden')) {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+  } else {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+  }
+  $(this).next('.card-profile-stats-more-content').slideToggle();
+});
+
+//JQuery and Ajax event listener
+$(function () {
+  $('#postLogin').on('submit', function (e) {
     e.preventDefault();
-    if ( $(".card-profile-stats-more-content").is(':hidden') ) {
-      $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
-    } else {
-      $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
-    }
-    $(this).next('.card-profile-stats-more-content').slideToggle();
+    var data = $(this).serialize();
+    console.log(data)
+    $.post('/usuarios/login', data, function (message) {
+      console.log(message)
+      if (message.message) {
+        alert(message.message);
+      } else if (message == 'OK') {
+        location.reload();
+      }
+    });
   });
+});
 
 //Validação de Senhas antes de levar para o servidor
 var senha = document.getElementById("senha");
 var confirmSenha = document.getElementById("confirmSenhaUser");
 
 function validarSenhas() {
-    // console.log("Senha: " + senha.value);
-    // console.log("Senha Confirm: " + confirmSenha.value);
-    if (senha.value != confirmSenha.value) {
-        confirmSenha.setCustomValidity("Senhas Diferentes!");
-        confirmSenha.style.borderColor="#f00";
-    } else {
-        confirmSenha.setCustomValidity("");
-        confirmSenha.style.borderColor="#ccc";
-    }
+  // console.log("Senha: " + senha.value);
+  // console.log("Senha Confirm: " + confirmSenha.value);
+  if (senha.value != confirmSenha.value) {
+    confirmSenha.setCustomValidity("Senhas Diferentes!");
+    confirmSenha.style.borderColor = "#f00";
+  } else {
+    confirmSenha.setCustomValidity("");
+    confirmSenha.style.borderColor = "#ccc";
+  }
 }
 confirmSenha.onkeyup = validarSenhas;
--- routes/nutriente/nutriente.js
+++ routes/nutriente/nutriente.js
@@ -5,11 +5,9 @@ require("../../src/models/entidades/nutriente");
 const Nutriente = mongoose.model("nutrientes");
 
 
-router.get('/nutrientes/', (req, res) =>{
-    Nutriente.find({Descricao: new RegExp(req.body.barraPesq,'i')}).then((nutrientes)=>{   
-        
-        console.log(nutrientes);
-        res.render("index", {nutrientes: nutrientes});
+router.get('', (req, res) =>{
+    Nutriente.find({Descricao: new RegExp(req.query.barraPesq,'i')}).then((nutrientes)=>{   
+    res.render("index", {nutrientes: nutrientes});
     }).catch((err)=>{
         req.flash("error_msg","houve um erro ao listar os nutrientes" );
         res.redirect("/");

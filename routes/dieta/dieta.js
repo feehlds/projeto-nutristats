3dc2092e555f38ecef68e4adebd3e30d4b6ba47b
André Nascimento de Freitas <afreitas@gertec.local>
2019-12-03 18:06:31 -0200

    criando tela de dieta

--- routes/dieta/dieta.js
+++ routes/dieta/dieta.js
@@ -5,23 +5,34 @@ require("../../src/models/entidades/dieta");
 const Dieta = mongoose.model("dietas");
 
 
+// lista de dietas relacioanada ao usuário logado
+router.get('/adicionar',(req,res)=>{
+  
+    var nutrientes = {
+         "_id":{"$id":"5de29e4da67cc26c68be8eae"},"tipoAlimento":"1","Descricao":"Arroz, integral, cozido","Umidade":"70,1","kcal":"124","kj":"517","Proteina":"2,6","Lipideos":"1,0","Colesterol":"NA","carboidrato":"25,8","FibraAlimentar":"2,7","Cinzas":"0,5","Calcio":"5","Magnesio":"59","Manganes":"0,63","Fosforo":"106","Ferro":"0,3","Sodio":"1","Potassio":"75","Cobre":"0,02","Zinco":"0,7","Retinol":"NA","RE":null,"RAE ":null,"Tiamina":"0,08","Riboflavina":"Tr","Piridoxina":"0,08","Niacina":"Tr","VitaminaC":null,
+        
+        }  
+    res.render("dietas/index", { nutrientes})
+});
 //cadastro de dieta
 router.post('/registro', (req, res) => {
-    const dietaNova = new Dieta({
-        descricao: req.body.descricao,
-        cafeManha: req.body.cafeManha,
-        almoco: req.body.almoco,
-        cafeTarde: req.body.cafeTarde,
-        janta: req.body.janta,
-        ativo: req.body.ativo,
-        Usuario: req.body.IdUsuario
-    });
+    console.log("entrou")
+ 
+    // const dietaNova = new Dieta({
+    //     descricao: req.body.descricao,
+    //     cafeManha: req.body.cafeManha,
+    //     almoco: req.body.almoco,
+    //     cafeTarde: req.body.cafeTarde,
+    //     janta: req.body.janta,
+    //     ativo: req.body.ativo,
+    //     Usuario: req.body.IdUsuario
+    // });
 
-    dietaNova.save().then(()=>{
-        res.status(201).json(dietaNova);
-    }).catch((error)=>{
-        res.sendStatus(404);
-    });
+    // dietaNova.save().then(()=>{
+    //     res.status(201).json(dietaNova);
+    // }).catch((error)=>{
+    //     res.sendStatus(404);
+    // });
 });
 
 // lista de dietas relacioanada ao usuário logado
